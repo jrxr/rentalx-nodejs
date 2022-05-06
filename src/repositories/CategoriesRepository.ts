53d7363d41a48885b5c847502ae0502552d2892a
Agacy junior <juniormartinssom@gmail.com>
2022-05-06 17:15:09 -0300

    Utilizando o princípio de responsabilidade única

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,10 @@
 import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
@@ -29,7 +28,7 @@ class CategoriesRepository {
   }
 
   findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+    const category = this.categories.find((category) => category.name === name);
     return category;
   }
 }
