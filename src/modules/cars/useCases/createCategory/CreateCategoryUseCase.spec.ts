a73e9ea5df78270764dd1baf6c382fe905597472
Agacy junior <juniormartinssom@gmail.com>
2022-05-12 22:03:45 -0300

    Teste de criação de categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoryRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoryRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoryRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category Description Test",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await categoryRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with already used name", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category Description Test",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    await expect(createCategoryUseCase.execute(category)).rejects.toEqual(
+      new AppError("Category already exists")
+    );
+  });
+});
