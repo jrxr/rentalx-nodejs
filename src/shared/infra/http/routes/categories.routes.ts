a2cd56861663dbca8eab0a9c6183554a265d8a09
Agacy junior <juniormartinssom@gmail.com>
2022-05-16 21:48:50 -0300

    Criando middleware de administrador

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,7 +4,8 @@ import { ListCategoriesController } from "@modules/cars/useCases/listCategories/
 
 import { Router } from "express";
 import multer from "multer";
-
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
@@ -16,13 +17,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const specificationsRouter = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRouter.use(ensureAuthenticated);
-specificationsRouter.post("/", createSpecificationController.handle);
+specificationsRouter.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRouter };
