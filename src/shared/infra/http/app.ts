5211d7717fc1f55898d401d83c7e1d3d629788d9
Agacy junior <juniormartinssom@gmail.com>
2022-05-24 11:29:11 -0300

    Teste de integração do controller category

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,71 @@
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import "express-async-errors";
+
+import "reflect-metadata";
+import "dotenv/config";
+
+import "../../container";
+
+import upload from "../../../config/upload";
+import swaggerFile from "../../../swagger.json";
+import { AppError } from "../../errors/AppError";
+import createConnection from "../typeorm";
+import { router } from "./routes";
+
+createConnection();
+
+const app = express();
+
+app.use(cors());
+
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,35 +1,5 @@
-import "reflect-metadata";
-import swaggerUi from "swagger-ui-express";
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
+import { app } from "./app";
 
-import createConnection from "@shared/infra/typeorm";
-
-import "./shared/container";
-import { AppError } from "../../errors/AppError";
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use((err: Error, request:Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error: ${err.message}`,
-  });
-});
-
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(3333, () =>
+  console.log("Server is running")
+);
